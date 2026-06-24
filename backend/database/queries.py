016c3f4c0d7f486dce37efabac8add236a675f91
Aditya Thakur <thakuradityadev9336@gmail.com>
2026-06-24 15:50:20 +0530

    Update DB query and retailer service layer

--- backend/database/queries.py
+++ backend/database/queries.py
@@ -1,17 +1,34 @@
 from sqlalchemy import text
 from database.connection import engine
 
-def get_retailers_by_district(district:str):
+def get_retailers_by_district_and_product(district:str, product:str):
     query = text("""
-        SELECT id, name, address, phone
-                 FROM retailers
-                 WHERE district = :district
+        SELECT
+            r.id,
+            r.name,
+            r.address,
+            r.phone,
+            p.name AS product_name,
+            s.quantity
+
+        FROM retailers r
+
+        JOIN stock s
+            ON r.id = s.retailer_id
+
+        JOIN products p
+            ON p.id = s.product_id
+
+        WHERE
+            r.district = :district
+            AND p.name = :product
+            AND s.quantity > 0
     """)
 
     with engine.connect() as conn:
         result = conn.execute(
             query,
-            {"district": district}
+            {"district": district, "product": product}
         )
         rows = result.fetchall()
 
--- backend/services/retailer_service.py
+++ backend/services/retailer_service.py
@@ -1,4 +1,4 @@
-from database.queries import get_retailers_by_district
+from database.queries import get_retailers_by_district_and_product
 
-def search_retailers_by_district(district:str):
-    return get_retailers_by_district(district)
+def search_retailers_by_district(district:str, product:str):
+    return get_retailers_by_district_and_product(district, product)
