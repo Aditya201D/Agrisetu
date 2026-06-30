d2321dadcc4d2459242b474787c0939be9b7c453
Aditya Thakur <thakuradityadev9336@gmail.com>
2026-06-30 12:49:29 +0530

    Add more UI-facing menus and rename file

---
+++ backend/state_machine/ui_text.py
@@ -0,0 +1,53 @@
+SEARCH_MODE_MENU = (
+    "How would you like to search?\n\n"
+    "1. By District\n"
+    "2. Near Me"
+)
+
+PRODUCT_MENU = (
+    "Choose a product:\n"
+    "1. Urea\n"
+    "2. DAP\n"
+    "3. NPKs\n"
+    "4. SSP\n"
+    "5. MOP\n"
+    "6. FOM\n"
+    "7. All"
+)
+
+POST_RESULTS_MENU = (
+    "--------------------------------\n"
+    "What would you like to do next?\n\n"
+    "1. New Search\n"
+    "2. Change Product\n"
+    "3. Change Area\n"
+    "4. Done"
+)
+
+DISTRICT_PROMPT = (
+    "Please select or enter your district."
+)
+
+LOCATION_PROMPT = (
+    "Please share your location."
+)
+
+RADIUS_MENU = (
+    "Select a search radius:\n\n"
+    "1. 25 km\n"
+    "2. 50 km\n"
+    "3. 100 km"
+)
+
+NO_RESULTS_MESSAGE = (
+    "No inventory found.\n\n"
+    "Try another product or area."
+)
+
+SERVER_ERROR_MESSAGE = (
+    "Something went wrong.\nPlease try again."
+)
+
+SESSION_EXPIRED_MESSAGE = (
+    "Your session has expired.\nPlease log in again."
+)
