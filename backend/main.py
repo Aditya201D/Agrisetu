264c7eb6c769d5a2c186920e9bffb70c2c605ee9
Aditya Thakur <thakuradityadev9336@gmail.com>
2026-07-05 20:15:47 +0530

    Cache LLM for first prompt in each session

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,25 @@
+from contextlib import asynccontextmanager
+from llm.client import ask_llm
 from fastapi import FastAPI
 from routes.chat import router as chat_router
 from fastapi.middleware.cors import CORSMiddleware
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Loading local LLM...")
+
+    try:
+        ask_llm("hello")
+    except Exception as e:
+        print(e)
+
+    print("LLM ready.")
+
+    yield
+
 app = FastAPI(
-    title = "AgriSetu"
+    title="AgriSetu",
+    lifespan=lifespan,
 )
 
 app.add_middleware(
